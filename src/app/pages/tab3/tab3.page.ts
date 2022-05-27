5be2d016085e194d9d1b7bc9e14f6c168c8f9ec4
jrbistuer <jrbistuer@gmail.com>
2022-05-27 14:46:16 +0200

    maps

--- src/app/pages/tab3/tab3.page.ts
+++ src/app/pages/tab3/tab3.page.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GoogleMap } from '@capacitor/google-maps';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
   constructor() {}
+  ngOnInit(): void {
+    this.createMap();
+  }
+
+  async createMap() {
+    const myApiKey = '';
+    const mapElement = document.getElementById('my-map');
+    const mapConfig = {
+      center: {
+        lat: 33.6,
+        lng: -117.9,
+      },
+      zoom: 8,
+      androidLiteMode: false,
+    };
+    const mapOptions = {
+      id: 'map',
+      apiKey: myApiKey,
+      config: mapConfig,
+      element: mapElement,
+    };
+
+    // Create the Map Element
+    const map = await GoogleMap.create(mapOptions);
+
+    await map.addMarkers([{
+      coordinate: {
+        lat: 33,
+        lng: 117,
+      },
+      title: 'Hello world',
+    }]);
+
+  }
 
 }
