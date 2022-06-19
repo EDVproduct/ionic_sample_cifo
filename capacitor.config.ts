33b8c64fcc95329299bbced041f326222b8dc36d
jrbistuer <jrbistuer@gmail.com>
2022-06-19 18:30:09 +0200

    splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,25 @@ const config: CapacitorConfig = {
   appId: 'com.jrbistuer.cifo',
   appName: 'cifo',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    SplashScreen: {
+      launchShowDuration: 4000,
+      launchAutoHide: true,
+      backgroundColor: '#ffffff',
+      androidSplashResourceName: 'splash',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: true,
+      androidSpinnerStyle: 'large',
+      iosSpinnerStyle: 'small',
+      spinnerColor: '#999999',
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: 'launch_screen',
+      useDialog: false,
+    }
+  }
 };
 
 export default config;
